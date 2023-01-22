35100dc0401b44815f2ce024ea59327c49af2906
Batuhan A <batuhana@Batuhans-MBP.home>
2023-01-22 06:42:08 +0300

    Dependency management

---
+++ api-gateway/src/test/java/com/baakapp/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baakapp.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ range-service/src/test/java/com/baakapp/rangeservice/RangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baakapp.rangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
