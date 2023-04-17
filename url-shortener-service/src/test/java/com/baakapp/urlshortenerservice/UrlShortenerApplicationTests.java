1873a4e3098e0a3ac4c5f2ffc94983b192de4305
baakapps <baksudeveloper@gmail.com>
2023-04-17 06:51:29 +0300

    Added Testcontainers in UrlShortenerApplicationTests

--- url-shortener-service/src/test/java/com/baakapp/urlshortenerservice/UrlShortenerApplicationTests.java
+++ url-shortener-service/src/test/java/com/baakapp/urlshortenerservice/UrlShortenerApplicationTests.java
@@ -1,20 +1,33 @@
 package com.baakapp.urlshortenerservice;
 
-import com.baakapp.urlshortenerservice.controller.UrlController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-@SpringBootTest(classes = UrlShortenerApplicationTests.class)
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UrlShortenerApplicationTests {
-    @Autowired
-    private UrlController urlController;
+    @Container
+    static final GenericContainer mongo = new GenericContainer("mongo:6.0.3")
+                .withExposedPorts(27017)
+                .withEnv("MONGO_INITDB_ROOT_USERNAME", "root")
+                .withEnv("MONGO_INITDB_ROOT_PASSWORD", "rootpassword");
+
+    @DynamicPropertySource
+    static void mongoProperties(org.springframework.test.context.DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.host", mongo::getHost);
+        registry.add("spring.data.mongodb.port", mongo::getFirstMappedPort);
+        registry.add("spring.data.mongodb.database", () -> "app_db");
+        registry.add("spring.data.mongodb.username", () -> "root");
+        registry.add("spring.data.mongodb.password", () -> "rootpassword");
+        registry.add("spring.data.mongodb.authentication-database", () -> "admin");
+        registry.add("spring.data.mongodb.auto-index-creation", () -> "true");
+    }
 
     @Test
-    void contextLoads() {
-        assertNotNull(urlController);
-    }
+    void contextLoads() {}
 
 }
