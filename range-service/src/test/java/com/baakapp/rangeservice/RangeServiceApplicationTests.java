8dd56ba945900c80cb44d01ca29f6a56dcd0bb5c
baakapps <baksudeveloper@gmail.com>
2023-04-17 06:51:18 +0300

    Added Testcontainers in RangeServiceApplicationTests

--- range-service/src/test/java/com/baakapp/rangeservice/RangeServiceApplicationTests.java
+++ range-service/src/test/java/com/baakapp/rangeservice/RangeServiceApplicationTests.java
@@ -1,20 +1,36 @@
 package com.baakapp.rangeservice;
 
-import com.baakapp.rangeservice.controller.RangeController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RangeServiceApplicationTests {
-    @Autowired
-    private RangeController rangeController;
 
-    @Test
-    void contextLoads() {
-        assertNotNull(rangeController);
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.28")
+            .withUsername("root")
+            .withPassword("rootpasword")
+            .withDatabaseName("url_shortener")
+            .withConnectTimeoutSeconds(30)
+            .withReuse(true);
+
+
+    @DynamicPropertySource
+    static void mysqlProperties(org.springframework.test.context.DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.jdbcUrl", mysql::getJdbcUrl);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+        registry.add("spring.jpa.show-sql=true", () -> true);
+        registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQLDialect");
     }
 
+    @Test
+    void contextLoads () {}
 }
