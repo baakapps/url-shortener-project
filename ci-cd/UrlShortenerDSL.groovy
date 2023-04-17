2cac83c58ff56bf04be0ce28da18fed16c11ee37
baakapps <baksudeveloper@gmail.com>
2023-04-17 18:18:08 +0300

    Updated DSL file

--- ci-cd/UrlShortenerDSL.groovy
+++ ci-cd/UrlShortenerDSL.groovy
@@ -1,15 +1,30 @@
-job('url-shortener-project') {
+pipelineJob('url-shortener-dsl') {
     description("Url shortener project")
     scm {
         git("https://github.com/baakapps/url-shortener-project.git", 'master')
     }
+    definition {
+        cps {
+            sandbox()
+            script(readFileFromWorkspace('Jenkinsfile'))
+            stages {
+                stage('Build') {
+                    steps {
+                        maven {
+                            goals('clean package')
+                            jdk('maven-default')
+                        }
+                        archiveArtifacts('**/*.jar')
+                    }
+                }
+            }
+        }
+    }
+    parameters {
+        booleanParam(name: 'runAllServices', defaultValue: true, description: 'Whether to run all services or select some of them')
+        choiceParam(name: 'selectedServices', choices: ['serviceA', 'serviceB', 'serviceC'], description: 'Select the services to run', multiSelect: true, filterable: true)
+    }
     triggers {
         scm('* * * * *')
-    }
-    steps {
-        maven('clean package', 'pom.xml')
-    }
-    publishers {
-        archiveArtifacts '**/*.jar'
     }
 }
